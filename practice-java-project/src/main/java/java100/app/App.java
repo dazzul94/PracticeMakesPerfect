35e2de27c547fb9e2857d1f1fb60a77c1fa69dbe
dazzul94 <dazzul@naver.com>
2017-11-06 18:53:36 +0900

    .

--- practice-java-project/src/main/java/java100/app/App.java
+++ practice-java-project/src/main/java/java100/app/App.java
@@ -1,103 +1,152 @@
+//: ## ver 23
+//: - 게시판 기능을 추가하라!
+//: - 자세한 요구사항은 App.java의 주석을 확인하라!
+//: - 학습목표
+//:   - 클래스를 만들고 메서드를 정의하는 것을 연습한다.
+//:   - 복/붙 연습!
+//: 
+//: 
+//: 요구사항 
+/*
+게시판> add
+번호? 1
+제목? aaa
+내용? aaaa
+입력하였습니다.
+
+게시판> add
+번호? 1
+이미 등록된 번호입니다.
+
+게시판> list
+1, aaa, 2017-11-3, 0
+2, bbb, 2017-11-3, 2
+
+게시판> view
+번호? 1
+제목: aaa
+내용: aaaa
+등록일: 2017-11-3
+조회수: 1   <== view 할 때 마다 해당 게시물의 조회수는 증가한다.
+
+게시판> view
+번호? 9
+9번 게시물이 없습니다.
+
+게시판> delete
+번호? 1
+정말 삭제하시겠습니까?(y/N) y <== 'Y', 'y'를 입력해야 삭제된다.
+삭제하였습니다.
+
+게시판> delete
+번호? 1
+정말 삭제하시겠습니까?(y/N) n
+삭제취소하였습니다.
+
+게시판> delete
+번호? 9
+9번 게시물이 없습니다.
+
+게시판> update
+번호? 1
+제목?(aaa) xxxx    <== 엔터를 치면 원래 값 유지
+내용? 2222  <== 입력하지 않고 그냥 엔터를 치면 빈 문자열이 내용이 된다.
+변경하시겠습니까?(y/N) y<== 'Y', 'y'를 입력해야 변경된다.
+변경하였습니다.   <== 변경할 때 등록일을 현재 날짜 및 시작으로 바꿔라
+
+게시판> update
+번호? 1
+제목?(aaa) xxxx    <== 엔터를 치면 원래 값 유지
+내용? 2222  <== 입력하지 않고 그냥 엔터를 치면 빈 문자열이 내용이 된다.
+변경하시겠습니까?(y/N) n<== 'Y', 'y'를 입력해야 변경된다.
+변경취소하였습니다.
+
+게시판> update
+번호? 9
+9번 게시물이 없습니다.
+
+게시판> remove
+수행할 수 없는 명령입니다.
+
+게시판> main
+
+명령>
+ */
 package java100.app;
-import java.util.ArrayList;
-import java.util.Iterator;
+
 import java.util.Scanner;
-
+ 
+// 
+// 
 public class App {
-    private static boolean confirm(String message) {
-        Scanner keyScan = new Scanner(System.in);
-        System.out.print(message);
-        String input = keyScan.nextLine().toLowerCase();
-        if(input.equals("y")|| input.equals("yes"))
-            return true;
-        return false;
-    }
+    
+    static Scanner keyScan = new Scanner(System.in);
+    static ScoreController scoreController = new ScoreController();
+    static MemberController memberController = new MemberController();
+    static BoardController boardController = new BoardController();
+    
     public static void main(String[] args) {
-        ArrayList<Score> list = new ArrayList<>();
-        Scanner keyScan = new Scanner(System.in);
-        Iterator<Score> iterator;
-        Loop:
-            while(true) {
-                System.out.print("성적관리>");
-                String input = keyScan.nextLine();
-                switch(input) {
-                case "add" : 
-                    System.out.println("[성적 등록]");
-
-                    while(true) {
-                        Score score = new Score();
-                        score.input();
-                        list.add(score);
-
-                        if(!confirm("계속 하시겠습니까?(Y/n) ")) {
-                            break;
-                        }
-                    }
-                    break;
-
-                case "list" : 
-                    System.out.println("[성적 목록]");
-                    iterator = list.iterator();
-                    while(iterator.hasNext())
-                        iterator.next().print();
-                    break;
-
-                case "view" : 
-                    System.out.println("[성적 정보]");
-
-                    System.out.print("이름? ");
-                    input = keyScan.nextLine();
-
-                    Score score = null;
-                    iterator = list.iterator();
-
-                    while(iterator.hasNext()) {
-                        
-                        if (iterator.next().name.equals(input)) {
-                            score = iterator.next();
-                            break; //찾으면 나간다!
-                        }  
-                    }
-                    if (score == null) {
-                        System.out.printf("%s 의 성적 정보가 없습니다\n",input);
-                    } else {
-                        score.print();
-                    }
-                    break;
-                case "delete" : System.out.println("delete");
-                break;
-                case "update" : System.out.println("update");
-                break;
-                case "quit" : System.out.println("quit");
-                break Loop;
-                default: System.out.println("수행할 수 없는 명령입니다"); 
+        
+        loop:
+        while (true) {
+            System.out.print("명령> ");
+            String[] input = keyScan.nextLine().toLowerCase().split(" ");
+            
+            try {
+                switch (input[0]) {
+                case "menu": doMenu(); break;
+                case "help": doHelp(); break;
+                case "quit": doQuit(); break loop;
+                case "go": doGo(input[1]); break;
+                default:
+                    doError();
                 }
-                System.out.println();
+            } catch (Exception e) {
+                System.out.println("명령 처리 중 오류 발생!");
+                e.printStackTrace();
             }
-
-
-
-
-        /*  ArrayList<Score> list = new ArrayList<>();
-
-        while(true) {
-
-            Score score = new Score();
-            score.input();
-            list.add(score);
-
-            if(!confirm("계속 하시겠습니까?")) {
-                break;
-            }
+            System.out.println();
+        } // while
+        
+    }
+    
+    private static void doGo(String menuNo) {
+        
+        switch (menuNo) {
+        case "1": scoreController.execute(); break;
+        case "2": memberController.execute(); break;
+        case "3": boardController.execute(); break;
+        default:
+            System.out.println("해당 번호의 메뉴가 없습니다.");
         }
-
-        Iterator<Score> iterator = list.iterator();
-        while(iterator.hasNext()) {
-            iterator.next().print();
-        }
-         */
+        
     }
 
+    private static void doHelp() {
+        System.out.println("[명령]");
+        System.out.println("menu        - 메뉴 목록 출력한다.");
+        System.out.println("go 번호     - 메뉴로 이동한다.");
+        System.out.println("quit        - 프로그램을 종료한다.");
+    }
+
+    private static void doMenu() {
+        System.out.println("1 성적관리");
+        System.out.println("2 회원관리");
+        System.out.println("3 게시판");
+    }
+
+    private static void doError() {
+        System.out.println("실행할 수 없는 명령입니다.");
+    }
+
+    private static void doQuit() {
+        System.out.println("프로그램을 종료합니다.");
+    }
+
+
+
+
+        
 }
 
 
--- practice-java-project/src/main/java/java100/app/Score.java
+++ practice-java-project/src/main/java/java100/app/Score.java
@@ -1,11 +1,32 @@
+//: ## 캡슐화 적용
+//: - 모든 필드에 대해 외부 접근을 차단한다.
+//:   단 자식 클래스에서는 직접 접근할 수 있게 허락한다.
+//: 
 package java100.app;
 import java.util.Scanner;
 
-public class Score {
+public class Score {  
+    
     protected String name;
-    protected int[] subjects = new int[3];
+    protected int kor; 
+    protected int eng; 
+    protected int math; 
     protected int sum;
     protected float aver;
+    
+    
+
+    //: ### 생성자
+    //: > 다른 패키지에서도 호출할 수 있도록 public으로 공개한다.
+    public Score() {}
+    
+    public Score(String name, int kor, int eng, int math) {
+        this.name = name;
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
+        this.compute();
+    }
     
     public String getName() {
         return name;
@@ -13,59 +34,47 @@ public class Score {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getKor() {
+        return kor;
+    }
+
+    public void setKor(int kor) {
+        this.kor = kor;
         this.compute();
     }
 
-    public int[] getSubjects() {
-        return subjects;
+    public int getEng() {
+        return eng;
     }
 
-    public void setSubjects(int kor, int eng, int math) {
-        this.subjects[0] = kor;
-        this.subjects[1] = eng;
-        this.subjects[2] = math;
-    }
-
-    Score() {}
-    
-    Score(String name, int kor, int eng, int math) {
-        this.name = name;
-        this.subjects[0] = kor;
-        this.subjects[1] = eng;
-        this.subjects[2] = math;
-        
+    public void setEng(int eng) {
+        this.eng = eng;
         this.compute();
     }
-    
-    void print() {
-        System.out.printf("%-4s, %4d, %4d, %4d, %4d, %6.1f\n",  
-                this.name, 
-                this.subjects[0], 
-                this.subjects[1],
-                this.subjects[2],
-                this.sum,
-                this.aver);
+
+    public int getMath() {
+        return math;
     }
+
+    public void setMath(int math) {
+        this.math = math;
+        this.compute();
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public float getAver() {
+        return aver;
+    }
+
+    //: > 내부에서만 사용할 메서드이기 때문에 공개하지 않는다.
     private void compute() {
-        for(int sub : this.subjects) {
-            this.sum += sub;
-            this.aver = this.sum / (float)this.subjects.length;
-        }
+        int sum = 0;
+        this.sum = this.kor + this.eng + this.math;
+        this.aver = (float)this.sum / 3f;
     }
-    void input() {
-        Scanner keyScan = new Scanner(System.in);
-        System.out.print("이름? ");
-        this.name = keyScan.nextLine();
-        System.out.print("국어? ");
-        this.subjects[0] = keyScan.nextInt();
-        System.out.print("영어? ");
-        this.subjects[1] = keyScan.nextInt();
-        System.out.print("수학? ");
-        this.subjects[2] = keyScan.nextInt();
-        
-        this.compute();
-        
-        
-    }
-
 }
