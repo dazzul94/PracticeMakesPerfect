f9e05125a724d00d78e17984ba1268e8b0d6dd87
dazzul94 <dazzul@naver.com>
2017-11-13 09:22:40 +0900

    .

---
+++ practice-java-basic/src/main/java/bitcamp/java100/ch14/ex2/Test1_2.java
@@ -0,0 +1,10 @@
+package bitcamp.java100.ch14.ex2;
+
+import java.io.FileInputStream;
+
+public class Test1_2 {
+    public static void main(String[] args) {
+        FileInputStream in = new FileInputStream("sample/)
+    }
+
+}
---
+++ practice-java-project/src32_1/app/control/Controller.java
@@ -0,0 +1,9 @@
+package java100.app.control;
+
+public interface Controller {
+    void execute();
+    void save();
+    void load();
+    
+
+}
