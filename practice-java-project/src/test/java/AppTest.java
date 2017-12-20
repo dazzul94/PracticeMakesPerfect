56c7dbcb45436823ff51b953dffba0a8849b41c3
dazzul94 <dazzul94@naver.com>
2017-12-20 16:19:36 +0900

    .

--- practice-java-project/src/test/java/AppTest.java
+++ practice-java-project/src/test/java/AppTest.java
@@ -3,13 +3,9 @@
  */
 import org.junit.Test;
 
-import java100.app.App;
-
-import static org.junit.Assert.*;
-
 public class AppTest {
     @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
+//        App classUnderTest = new App();
 //        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
 }
